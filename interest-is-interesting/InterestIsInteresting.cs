1ac668dab448a18cd9e6ad809ffd1c6256c82ce2
Richard Leer <richard.leer@parshipgroup.com>
2024-02-10 22:10:43 +0100

    Add InterestIsInteresting  code

---
+++ interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,27 @@
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance) => balance switch
+    {
+        <
+            0 => 3.213f,
+        < 1000 => 0.5f,
+        < 5000 => 1.621f,
+        >= 5000 => 2.475f,
+    };
+
+    public static decimal Interest(decimal balance) => balance * ((decimal)InterestRate(balance)/100);
+
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        var years = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+
+        return years;
+    }
+}
