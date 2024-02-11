3184f0e286ad86c41614bc1c4f3fab837b60375f
Richard Leer <richard.leer@parshipgroup.com>
2024-02-12 00:00:21 +0100

    Add solution for international-calling-connoisseur

--- international-calling-connoisseur/InternationalCallingConnoisseur.cs
+++ international-calling-connoisseur/InternationalCallingConnoisseur.cs
@@ -1,54 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class DialingCodes
 {
-    public static Dictionary<int, string> GetEmptyDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
-    }
+    public static Dictionary<int, string> GetEmptyDictionary() => new();
 
-    public static Dictionary<int, string> GetExistingDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
-    }
+    public static Dictionary<int, string> GetExistingDictionary() => new() { { 1, "United States of America" }, { 55, "Brazil" }, { 91, "India" } };
 
-    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
-    }
+    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName) => new() { { countryCode, countryName } };
 
     public static Dictionary<int, string> AddCountryToExistingDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
-    }
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName) => existingDictionary
+        .Append(new KeyValuePair<int, string>(countryCode, countryName)).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
     public static string GetCountryNameFromDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
-    }
+        Dictionary<int, string> existingDictionary, int countryCode) =>
+        existingDictionary.ContainsKey(countryCode) ? existingDictionary[countryCode] : string.Empty;
 
-    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
-    }
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode) => existingDictionary.ContainsKey(countryCode);
 
     public static Dictionary<int, string> UpdateDictionary(
         Dictionary<int, string> existingDictionary, int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
+        if (existingDictionary.ContainsKey(countryCode)) existingDictionary[countryCode] = countryName;
+        return existingDictionary;
     }
+
 
     public static Dictionary<int, string> RemoveCountryFromDictionary(
         Dictionary<int, string> existingDictionary, int countryCode)
     {
-        throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
     }
 
     public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
     {
-        throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
+        (string country, int length) longestCountryNamePair = (string.Empty, 0);
+        foreach (var kvp in existingDictionary)
+        {
+            if (kvp.Value.Length > longestCountryNamePair.length)
+                longestCountryNamePair = (kvp.Value, kvp.Value
+                    .Length);
+        }
+
+        return longestCountryNamePair.country;
     }
 }
