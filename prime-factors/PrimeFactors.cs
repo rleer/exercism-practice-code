f47a999aa0e692664d714bf32493b41c0520d6af
Richard Leer <richard.leer@parshipgroup.com>
2024-02-11 17:59:21 +0100

    Add solution for prime-factors

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -1,9 +1,24 @@
-using System;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
     public static long[] Factors(long number)
     {
-        throw new NotImplementedException();
+        if (number == 1) return new long[] { };
+        var divisor = 2;
+        var primeFactors = new List<long>();
+        while (number > 1)
+        {
+            if (number % divisor == 0)
+            {
+                number /= divisor;
+                primeFactors.Add(divisor);
+            }
+            else
+            {
+                divisor++;
+            }
+        }
+        return primeFactors.ToArray();
     }
 }
