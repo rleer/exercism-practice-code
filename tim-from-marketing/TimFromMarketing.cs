7593a9d86a6e0adf353c7c242aba80e9fbec78a0
Richard Leer <richard.leer@parshipgroup.com>
2024-02-13 10:04:41 +0100

    Add solution for tim-from-marketing

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -1,9 +1,8 @@
 using System;
+using System.Linq;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+    public static string Print(int? id, string name, string? department) => 
+        id != null ? $"[{id}] - {name} - {department?.ToUpperInvariant() ?? "OWNER"}" : $"{name} - {department?.ToUpperInvariant() ?? "OWNER"}";
 }
