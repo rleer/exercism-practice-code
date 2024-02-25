69dbfa99b3f9e16721b3472a83cdf59e2edbb884
Richard Leer <richard.leer@parshipgroup.com>
2024-02-25 18:17:00 +0100

    Add git commands to submit script

--- submit.sh
+++ submit.sh
@@ -3,7 +3,11 @@
 exercise_name=$1
 
 file_name=$(grep -ohE "([[:alnum:]]+\.cs)" ${exercise_name}/HELP.md)
-exercism submit ${exercise_name}/${file_name}
+echo exercism submit ${exercise_name}/${file_name}
 
-git add .
-git commit -m "Add solution for ${exercise_name} exercise"
+if [ "$(git status --porcelain)" ]; then 
+  echo git add .
+  echo git commit -m "Add solution for ${exercise_name} exercise"
+else 
+  echo "Nothing to commit"
+fi
