8052d511885223b73ad33dc7f5eda853c6f9c39d
Richard Leer <richard.leer@parshipgroup.com>
2024-02-25 17:20:39 +0100

    Add solution for calculator-conundrum exercise

--- calculator-conundrum/CalculatorConundrum.cs
+++ calculator-conundrum/CalculatorConundrum.cs
@@ -2,8 +2,14 @@ using System;
 
 public static class SimpleCalculator
 {
-    public static string Calculate(int operand1, int operand2, string operation)
-    {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
-    }
+    public static string Calculate(int operand1, int operand2, string operation) =>
+        operation switch
+        {
+            "+" => $"{operand1} + {operand2} = {SimpleOperation.Addition(operand1, operand2)}",
+            "/" => operand2 == 0 ? "Division by zero is not allowed." : $"{operand1} / {operand2} = {SimpleOperation.Division(operand1, operand2)}",
+            "*" => $"{operand1} * {operand2} = {SimpleOperation.Multiplication(operand1, operand2)}",
+            null => throw new ArgumentNullException(),
+            "" => throw new ArgumentException(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
 }
