842388997d9f7c0029b2700431d70c86cef63278
Richard Leer <richard.leer@parshipgroup.com>
2024-02-25 15:32:36 +0100

    Add improved solution for word-search

--- word-search/WordSearch.cs
+++ word-search/WordSearch.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 public class WordSearch
 {
-    private string[] _grid;
-    private int _gridRows { get => _grid.GetLength(0); }
-    private int _gridColumns { get => _grid[0].Length; }
+    private readonly string[] _grid;
+    private int GridRows { get => _grid.GetLength(0); }
+    private int GridColumns { get => _grid[0].Length; }
     private readonly (int x, int y)[] _directions = new[]
     {
         ( 0,-1), // up
@@ -20,44 +20,33 @@ public class WordSearch
     
     public WordSearch(string grid) => _grid = grid.Split('\n');
 
-    public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor)
-    {
-        var searchResult = wordsToSearchFor.ToDictionary<string, string,((int, int), (int, int))?> (key => key, _ => null);
+    public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor) =>
+        wordsToSearchFor.ToDictionary(word => word, SearchForWord);
+
+    private ((int, int), (int, int))? SearchForWord(string word) 
+        => _grid
+            .SelectMany((line, y) => line.Select((c, x) => (c, x, y)))
+            .Where(point => 
+                point.c == word[0])
+            .SelectMany(point => _directions.Select(direction => SearchInGrid(word, point.x, point.y, direction)))
+            .FirstOrDefault(o => o is not null);
         
-        for (int yCord = 0; yCord < _gridRows; yCord++)
-        {
-            for (int xCord = 0; xCord < _gridColumns; xCord++)
-            {
-                foreach (var targetWord in wordsToSearchFor)
-                {
-                    if (targetWord[0] != _grid[yCord][xCord])
-                        continue;
-                    var endCord = SearchInGrid(targetWord, xCord, yCord);
-                    if (endCord != null)
-                        searchResult[targetWord] = ((xCord + 1, yCord + 1), (endCord.Value.endX + 1, endCord.Value.endY + 1));
-                }
-            }
-        }
-
-        return searchResult;
-    }
-
-    private bool inBounds(int x, int y) => (y >= 0 && y < _grid.Length) && (x >= 0 && x < _grid[y].Length);
+    private bool IsInGrid(int x, int y) => (y >= 0 && y < GridRows) && (x >= 0 && x < GridColumns);
     
-    private (int endX, int endY)? SearchInGrid(string target, int startX, int startY)
+    private ((int, int), (int, int))? SearchInGrid(string word, int x, int y, (int dx, int dy) direction)
     {
-        foreach (var direction in _directions)
-        {
-            int i = 0, x = startX, y = startY;
-            while (i < target.Length && inBounds(x, y))
-            {
-                if (target[i] != _grid[y][x]) break; 
-                if (i == target.Length - 1) return (x, y);
-                x += direction.x;
-                y += direction.y;
-                i++;
-            }
-        }
-        return null;
+        var coords = word
+            .Select((c, i) => 
+                (c, cx: x + i * direction.dx, cy: y + i * direction.dy))
+            .ToArray();
+        
+        var valid = coords.All(cord => 
+            IsInGrid(cord.cx, cord.cy) && cord.c == _grid[cord.cy][cord.cx]);
+
+        if (!valid)
+            return null;
+
+        var end = coords.Last();
+        return ((x + 1, y + 1), (end.cx + 1, end.cy + 1));
     }
 }
