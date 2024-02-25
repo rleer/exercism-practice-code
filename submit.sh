c98a7134114928751d4f79f11113c6b8459fc8b2
Richard Leer <richard.leer@parshipgroup.com>
2024-02-25 18:17:28 +0100

    Remove debug echos

--- submit.sh
+++ submit.sh
@@ -3,11 +3,11 @@
 exercise_name=$1
 
 file_name=$(grep -ohE "([[:alnum:]]+\.cs)" ${exercise_name}/HELP.md)
-echo exercism submit ${exercise_name}/${file_name}
+exercism submit ${exercise_name}/${file_name}
 
 if [ "$(git status --porcelain)" ]; then 
-  echo git add .
-  echo git commit -m "Add solution for ${exercise_name} exercise"
+  git add .
+  git commit -m "Add solution for ${exercise_name} exercise"
 else 
   echo "Nothing to commit"
 fi
