2cfc99b2398f27011ac6f537d2b6060187dbaaad
Richard Leer <richard.leer@parshipgroup.com>
2024-02-25 19:05:01 +0100

    Add solution for attack-of-the-trolls exercise

--- attack-of-the-trolls/AttackOfTheTrolls.cs
+++ attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -8,31 +8,29 @@ internal enum AccountType
 }
 
 [Flags]
-internal enum Permission
+internal enum Permission : byte
 {
-    None = 0b000,
-    Read = 0b001,
-    Write = 0b010,
-    Delete = 0b100,
-    All = Read + Write + Delete
+    Read   = 1 << 0,
+    Write  = 1 << 1,
+    Delete = 1 << 2,
+    All    = Read | Write | Delete,
+    None   = 0,
 }
 
 static class Permissions
 {
-    public static Permission Default(AccountType accountType)
-    {
-        return accountType switch
+    public static Permission Default(AccountType accountType) =>
+        accountType switch
         {
             AccountType.Guest => Permission.Read,
             AccountType.User => Permission.Read | Permission.Write,
-            AccountType.Moderator => Permission.Read | Permission.Write | Permission.Delete,
+            AccountType.Moderator => Permission.All,
             _ => Permission.None
         };
-    }
 
     public static Permission Grant(Permission current, Permission grant) => current | grant;
 
-    public static Permission Revoke(Permission current, Permission revoke) => revoke == Permission.All? Permission.None : (current ^ revoke) & current;
+    public static Permission Revoke(Permission current, Permission revoke) => current & ~revoke; 
 
-    public static bool Check(Permission current, Permission check) => current >= check;
+    public static bool Check(Permission current, Permission check) => current.HasFlag(check);
 }
