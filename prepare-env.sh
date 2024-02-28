532cdfd969e60cbc151b65e323f8ec5a475156a8
Richard Leer <richard.leer@parshipgroup.com>
2024-02-28 17:58:00 +0100

    Automatically remove skip attributes from tests

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 exercise_name=$1
 
 # download code
@@ -8,6 +10,10 @@ exercism download --track=csharp --exercise=${exercise_name}
 # prepare SLN
 dotnet sln list | tail -1 | xargs dotnet sln remove
 dotnet sln add $(find ${exercise_name} -type f -name \*.csproj)
+
+# remove skip annotations in tests
+test_file=$(find ${exercise_name}  -type f -name \*Tests.cs)
+sed -i '' -E 's/\(Skip.+\)//g' ${test_file}
 
 # commit
 git add .
